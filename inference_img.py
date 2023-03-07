eda517bfbd1c2146b1b7de380a767854af16ac67
Bhagwat Garg <bhagwatgarg10@gmail.com>
2023-03-07 13:58:27 +0530

    training pipeline developed

--- inference_img.py
+++ inference_img.py
@@ -25,7 +25,7 @@ args = parser.parse_args()
 try:
     try:
         try:
-            from model.RIFE_HDv2 import Model
+            from model.RIFE import Model
             model = Model()
             model.load_model(args.modelDir, -1)
             print("Loaded v2.x HD model.")
